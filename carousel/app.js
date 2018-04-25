bb34ee75739f57447b048d807a52dda1e121a481
carotene512 <karoten512@gmail.com>
2018-04-25 23:29:59 +0900

    カルーセルの矢印にclickイベントを追加

--- carousel/app.js
+++ carousel/app.js
@@ -1,1 +1,14 @@
-// カルーセル実装
+// 矢印に対してclickイベントを実装
+var NAMESPACE = 'bs.carousel'
+
+var Selector = {
+  SLIDE: '[data-slide]'
+};
+
+var Event = {
+  CLICK: 'click' + '.' + NAMESPACE
+};
+
+$(document).on(Event.CLICK, Selector.SLIDE, function() {
+  console.log('スライド要素を見つけた');
+});
